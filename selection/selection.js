09989043354863babcd5b6c4168af6694561c05d
changshengee <1010875980@163.com>
2019-02-04 23:26:50 +0800

    :construction: 修改选择排序

--- selection/selection.js
+++ selection/selection.js
@@ -1,20 +1,19 @@
 var select = function () {
 
     function selection(arr) {
-        let size = vars.length;
+        let size = arr.length;
         var minIndex, temp;
         for (var i = 0; i < size - 1; i++) {
             minIndex = i;
-            for (var j = i + 1; j < size - 1; j++) {
-                if (vars[minindex] > arr[j]) {
+            for (var j = i + 1; j < size ; j++) {
+                if (arr[minIndex] > arr[j]) {
                     minIndex = j;
                 }
             }
             temp = arr[i];
-            arr[i] = arr[minindex];
+            arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
-
         return arr;
     }
 
